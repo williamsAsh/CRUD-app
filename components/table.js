1f452a02b3b634d5ab69001a7450b3e6e1b63b98
williamsashley <williams.a.law@gmail.com>
2022-11-24 20:41:32 +0900

    Pushing table.js
    Pushes changes made to crud app

---
+++ components/table.js
@@ -0,0 +1,36 @@
+export default function Table(){
+    return (
+
+        <table className="min-w-full table-auto">
+            <thread>
+                <tr className="bg-gray-800">
+                    <th className="px-16 py-2">
+                        <span className="text-gray-200">Name</span>
+                    </th>
+                </tr>
+                {/* <tr className="bg-gray-800">
+                    <th className="px-16 py-2">
+                        <span className="text-gray-200">Email</span>
+                    </th>
+                </tr>
+                <tr className="bg-gray-800">
+                    <th className="px-16 py-2">
+                        <span className="text-gray-200">Birthday</span>
+                    </th>
+                </tr>
+                <tr className="bg-gray-800">
+                    <th className="px-16 py-2">
+                        <span className="text-gray-200">Salary</span>
+                    </th>
+                </tr>
+                <tr className="bg-gray-800">
+                    <th className="px-16 py-2">
+                        <span className="text-gray-200">Actions</span>
+                    </th>
+                </tr> */}
+
+            </thread>
+        </table>
+    )
+
+}
